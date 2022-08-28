a1635898f2e44a6342260527fa4154bedbb7ad37
kvis21 <86234656+kvis21@users.noreply.github.com>
2022-08-28 11:16:07 +0300

    Update map.py

--- map.py
+++ map.py
@@ -41,20 +41,58 @@ def region(to_map):
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     #fig.show()
     return fig
-def label(train):
+def label(train, to_map):
     train=train.reset_index(drop=True)
-    m = folium.Map(
-        location=[-59.1759, -11.6016],
-        #tiles="open-street-map",
-        zoom_start=2,
-    )
-    folium.Choropleth(geo_data=geoJson, line_color='blue',
-                      line_weight=3).add_to(m)
-    for i in range(len(train)):
-        if i % 10 == 0:
-            folium.Marker(
-                location=[train['hex_lat'][i], train['hex_lon'][i]],
-                # tooltip=folium.Tooltip(text='dufha'),
-                icon=folium.Icon(color='red')
-            ).add_to(m)
-    return m
+
+    label1 = train[train.label == 1].reset_index(drop=True)
+    label0 = train[train.label == 0].reset_index(drop=True)
+
+    fig = go.Figure(go.Choroplethmapbox(geojson=geoJson,
+                                        locations=gdf['name'],
+                                        z=to_map['amount_connect'],
+                                        featureidkey="properties.name",
+                                        colorscale='greens',
+                                        # customdata=np.stack(train['inf_rate']),
+                                        zmin=0,
+                                        zmax=max(to_map['amount_connect']) + 100,
+                                        marker_opacity=0.5,
+                                        marker_line_width=1))
+    fig.update_layout(mapbox_style="open-street-map",
+                      mapbox_zoom=2, mapbox_center={"lat": 60.18678, "lon": 95.857324})
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+
+    site_lat = label1.hex_lat
+    site_lon = label1.hex_lon
+    locations_name = label1.city_name
+    fig.add_trace(go.Scattermapbox(
+        lat=site_lat,
+        lon=site_lon,
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=15,
+            color='rgb(0, 255, 0)',
+            opacity=0.7
+        ),
+        text=locations_name,
+
+        hoverinfo='text'
+    ))
+
+    site_lat0 = label0.hex_lat
+    site_lon0 = label0.hex_lon
+    locations_name0 = label0.city_name
+    fig.add_trace(go.Scattermapbox(
+        lat=site_lat0,
+        lon=site_lon0,
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=9,
+            color='rgb(255, 0, 0)',
+            opacity=0.5
+        ),
+        text=locations_name0,
+
+        hoverinfo='text'
+    ))
+    return fig
